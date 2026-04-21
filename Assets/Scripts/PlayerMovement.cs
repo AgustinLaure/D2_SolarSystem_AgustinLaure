de2913753ea6cad634fd38c1c98564b3db09e7d8
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-04-20 23:47:10 -0300

    Add physics based move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,66 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public enum MoveType
+    {
+        IsKinematic,
+        IsPhysicsBased
+    };
+
+
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private PlayerController controller;
+    [SerializeField] private MoveType moveType = MoveType.IsKinematic;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
 
+    [SerializeField] private const float maxTorque = 1f;
+    [SerializeField] private const float maxForce = 1f;
+
+    private const float torqueClamp = 50;
+    private const float forceClamp = 50;
+
     private void Update()
     {
-        Move();
+        if (Input.GetKey(KeyCode.K))
+        {
+            moveType = MoveType.IsKinematic;
+        }
+        else if (Input.GetKey(KeyCode.P))
+        {
+            moveType = MoveType.IsPhysicsBased;
+        }
+
+        if (moveType == MoveType.IsKinematic)
+        {
+            KinematicMove();
+        }
     }
-    private void Move()
+
+    private void FixedUpdate()
+    {
+        if (moveType == MoveType.IsPhysicsBased)
+        {
+            PhysicsBasedMove();
+        }
+    }
+    private void KinematicMove()
     {
         transform.rotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, controller.RotationDir);
 
         if (controller.IsAccel)
         {
             transform.position += transform.forward * (moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void PhysicsBasedMove()
+    {
+        rb.AddRelativeTorque(controller.RotationDir * rotationSpeed / torqueClamp, ForceMode.Force);
+
+        if (controller.IsAccel)
+        {
+            rb.AddForce(transform.forward * moveSpeed, ForceMode.Force);
         }
     }
 }
